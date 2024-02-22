3f17ac11f5b42634feede41288ac788252708b2c
leechiahwa <leechiahwa99@hotmail.com>
2024-02-22 23:42:49 +0800

    refactor: combine infoform components into single component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import "./App.css";
 import InfoForm from "./components/InfoForm";
-import EducationDetails from "./components/EducationDetails";
-import WorkDetails from "./components/WorkDetails";
-import CvPreview from "./components/CvPreview";
+
 function App() {
   return (
     <>
-        <div className="d-flex">
-          <div className="container">
-            <InfoForm />
-            <EducationDetails />
-            <WorkDetails />
-          </div>
-          {/*<CvPreview />*/}
+      <div className="d-flex">
+        <div className="container">
+          <InfoForm />
         </div>
+        {/*<CvPreview />*/}
+      </div>
     </>
   );
 }
--- src/components/InfoForm.tsx
+++ src/components/InfoForm.tsx
@@ -3,12 +3,21 @@ import { submitButton, editButton } from "./buttons";
 // import { useInfoForm } from "./InfoFormContext";
 
 export default function InfoForm() {
-  const [ person, setPerson ] = useState({
+  const [person, setPerson] = useState({
     name: "",
     email: "",
     age: "",
     phone: "",
-  })
+    schoolName: "",
+    courseName: "",
+    studyDateFrom: "",
+    studyDateTo: "",
+    companyName: "",
+    positionTitle: "",
+    mainResponsibility: "",
+    workDurationFrom: "",
+    workDurationTo: "",
+  });
 
   function handleNameChange(e) {
     setPerson({
@@ -37,6 +46,68 @@ export default function InfoForm() {
       phone: e.target.value,
     });
   }
+  function handleSchoolNameChange(e) {
+    setPerson({
+      ...person,
+      schoolName: e.target.value,
+    });
+  }
+
+  function handleCourseNameChange(e) {
+    setPerson({
+      ...person,
+      courseName: e.target.value,
+    });
+  }
+
+  function handleStudyDateFromChange(e) {
+    setPerson({
+      ...person,
+      studyDateFrom: e.target.value,
+    });
+  }
+
+  function handleStudyDateToChange(e) {
+    setPerson({
+      ...person,
+      studyDateTo: e.target.value,
+    });
+  }
+
+  function handleCompanyNameChange(e) {
+    setPerson({
+      ...person,
+      companyName: e.target.value,
+    });
+  }
+
+  function handlePositionTitleChange(e) {
+    setPerson({
+      ...person,
+      positionTitle: e.target.value,
+    });
+  }
+
+  function handleMainResponsibilityChange(e) {
+    setPerson({
+      ...person,
+      mainResponsibility: e.target.value,
+    });
+  }
+
+  function handleWorkDurationFromChange(e) {
+    setPerson({
+      ...person,
+      workDurationFrom: e.target.value,
+    });
+  }
+
+  function handleWorkDurationToChange(e) {
+    setPerson({
+      ...person,
+      workDurationTo: e.target.value,
+    });
+  }
 
   return (
     <>
@@ -45,12 +116,99 @@ export default function InfoForm() {
         <form className="d-flex flex-column p-2">
           Name:{" "}
           <input type="text" value={person.name} onChange={handleNameChange} />
-          Email: <input type="email" value={person.email} onChange={handleEmailChange}/>
-          Age: <input type="number" value={person.age} onChange={handleAgeChange}/>
-          Phone: <input type="tel" value={person.phone} onChange={handlePhoneChange}/>
+          Email:{" "}
+          <input
+            type="email"
+            value={person.email}
+            onChange={handleEmailChange}
+          />
+          Age:{" "}
+          <input type="number" value={person.age} onChange={handleAgeChange} />
+          Phone:{" "}
+          <input type="tel" value={person.phone} onChange={handlePhoneChange} />
         </form>
-        <button className="btn btn-secondary" onClick={editButton}>Edit</button>
-        <button className="btn btn-primary" onClick={submitButton}>Submit</button>
+        <button className="btn btn-secondary" onClick={editButton}>
+          Edit
+        </button>
+        <button className="btn btn-primary" onClick={submitButton}>
+          Submit
+        </button>
+      </div>
+      <div>
+        <h2>Education Form</h2>
+        <form className="d-flex flex-column p-2">
+          School Name:{" "}
+          <input
+            type="text"
+            value={person.schoolName}
+            onChange={handleSchoolNameChange}
+          />
+          Course Name:{" "}
+          <input
+            type="text"
+            value={person.courseName}
+            onChange={handleCourseNameChange}
+          />
+          Date of Study:{" "}
+          <input
+            type="date"
+            value={person.studyDateFrom}
+            onChange={handleStudyDateFromChange}
+          />{" "}
+          To{" "}
+          <input
+            type="date"
+            value={person.studyDateTo}
+            onChange={handleStudyDateToChange}
+          />
+        </form>
+        <button className="btn btn-secondary" onClick={editButton}>
+          Edit
+        </button>
+        <button className="btn btn-primary" onClick={submitButton}>
+          Submit
+        </button>
+      </div>
+      <div>
+        <h2>Work Info</h2>
+        <form className="d-flex flex-column p-2">
+          Company Name:{" "}
+          <input
+            type="text"
+            value={person.companyName}
+            onChange={handleCompanyNameChange}
+          />
+          Position Title:{" "}
+          <input
+            type="text"
+            value={person.positionTitle}
+            onChange={handlePositionTitleChange}
+          />
+          Main Responsibility:{" "}
+          <input
+            type="text"
+            value={person.mainResponsibility}
+            onChange={handleMainResponsibilityChange}
+          />
+          Work Duration:{" "}
+          <input
+            type="date"
+            value={person.workDurationFrom}
+            onChange={handleWorkDurationFromChange}
+          />{" "}
+          to{" "}
+          <input
+            type="date"
+            value={person.workDurationTo}
+            onChange={handleWorkDurationToChange}
+          />
+        </form>
+        <button className="btn btn-secondary" onClick={editButton}>
+          Edit
+        </button>
+        <button className="btn btn-primary" onClick={submitButton}>
+          Submit
+        </button>
       </div>
     </>
   );
