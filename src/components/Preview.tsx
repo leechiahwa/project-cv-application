3398dcc2da1a255dd5d05c98c2890c4e8ec77534
leechiahwa <leechiahwa99@hotmail.com>
2024-12-29 00:15:00 +0800

    Add info and styling in Preview

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -28,13 +28,51 @@ interface PreviewProps {
 
 function Preview({ personal, education, work }: PreviewProps) {
   return (
-    <div>
-      Resume Preview
-      <p>{personal.name}</p>
-      <p>{personal.phone}</p>
-      <p>{education.schoolName}</p>
-      <p>{work.companyName}</p>
+    <div className="preview-container">
+    <h2 className="preview-header">Resume Preview</h2>
+    <div className="section">
+      <h3>Personal Information</h3>
+      <p>
+        <strong>Name:</strong> {personal.name}
+      </p>
+      <p>
+        <strong>Email:</strong> {personal.email}
+      </p>
+      <p>
+        <strong>Age:</strong> {personal.age}
+      </p>
+      <p>
+        <strong>Phone:</strong> {personal.phone}
+      </p>
     </div>
+    <div className="section">
+      <h3>Education</h3>
+      <p>
+        <strong>School Name:</strong> {education.schoolName}
+      </p>
+      <p>
+        <strong>Course Name:</strong> {education.courseName}
+      </p>
+      <p>
+        <strong>Study Period:</strong> {education.studyDateFrom} - {education.studyDateTo}
+      </p>
+    </div>
+    <div className="section">
+      <h3>Work Experience</h3>
+      <p>
+        <strong>Company Name:</strong> {work.companyName}
+      </p>
+      <p>
+        <strong>Position Title:</strong> {work.positionTitle}
+      </p>
+      <p>
+        <strong>Main Responsibility:</strong> {work.mainResponsibility}
+      </p>
+      <p>
+        <strong>Work Duration:</strong> {work.workDurationFrom} - {work.workDurationTo}
+      </p>
+    </div>
+  </div>
   );
 }
 
